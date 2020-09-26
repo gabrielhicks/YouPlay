7d5ae6b13aab115fc2b72538e395985f4a65ff49
zhengjianliu <comezheng@gmail.com>
2020-09-26 16:17:01 -0400

    Display 50 songs

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,19 +3,15 @@ require 'rest-client'
 require 'json'
 require_relative '../config/environment'
 
+Video.destroy_all
 
-playlist_endpoint1 = RestClient.get(
-    'https://www.googleapis.com/youtube/v3/search?part=snippet
-                    &q=pop+music
-                    &type=playlist
-                    &key=AIzaSyCvYkzMg4rMqvzu6b6X10T1Yj283w391J4')
-binding.pry
+playlist = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=music&key=AIzaSyCvYkzMg4rMqvzu6b6X10T1Yj283w391J4')
 
-yt_hash = JSON.parse(playlist_endpoint1)
-
-
-#grab videoID
-yt_hash["items"][0]["id"]["videoId"]
-
-#grab PlaylistsTitle
-yt_hash["items"][0]["snippet"]["title"]
+# AIzaSyCvYkzMg4rMqvzu6b6X10T1Yj283w391J4
+youtube = JSON.parse(playlist)
+# binding.pry
+i=0
+50.times do
+  Video.create(video_id: youtube["items"][i]["id"]["videoId"], title: youtube["items"][i]["snippet"]["title"])
+  i+=1
+end
