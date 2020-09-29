f4ea32cf728fb12d3ff71fabcc6d4ad605e0f8b6
gabrielhicks <gabrielhicks@mail.com>
2020-09-29 19:05:21 -0400

    Fixed comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,10 @@ class CommentsController < ApplicationController
   def create
     @video = Video.find(params[:video_id]) 
     @comment = @video.comments.create(comment_params)
+    # respond_to do |format|
+    #   format.js
+    #   format.html {redirect_to comments_url}
+    # end
     # redirect_to video_path(@video)
   end
 
