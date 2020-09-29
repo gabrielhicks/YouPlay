9036ddc32da870f6bad3ce00575e7047e2ca34b8
gabrielhicks <gabrielhicks@mail.com>
2020-09-29 17:27:48 -0400

    AJAX/jQuery trouble

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,7 @@ class CommentsController < ApplicationController
   def create
     @video = Video.find(params[:video_id]) 
     @comment = @video.comments.create(comment_params)
-    redirect_back(fallback_location: videos_path(@video))
+    # redirect_to video_path(@video)
   end
 
   def edit
