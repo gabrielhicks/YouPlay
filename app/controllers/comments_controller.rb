8e2a96e64d98d508b354dcf78126580ff37c8cd1
gabrielhicks <gabrielhicks@mail.com>
2020-09-28 21:10:40 -0400

    gabe

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,31 +1,40 @@
 class CommentsController < ApplicationController
-  before_action :find_comment, only: [:edit, :update, :destroy]
+  # before_action :find_comment, only: [:edit, :update, :destroy]
 
   def new
     @comment = Comment.new
   end
 
   def create
-    comment = @current_user.comments.create(comment_params)
-
-    if comment.valid?
-      redirect_to video_path(@video)
-    else
-      flash[:errors] = comment.errors.full_messages
-      redirect_to video_path(@video)
-    end
+    # comment = @current_user.comments.create(comment_params)
+    # if comment.valid?
+    #   redirect_to video_path(@video)
+    # else
+    #   flash[:errors] = comment.errors.full_messages
+    #   redirect_to video_path(@video)
+    # end
+    @video = Video.find(params[:video_id]) # finds the video with the associated video_id
+    @comment = @video.comments.create(comment_params) # creates the comment on the video passing in params 
+    @comment.user_id = current_user.id if current_user # assigns logged in user's ID to comment
+    @comment.save!
+    redirect_to video_path(@video)
   end
 
   def edit
   end
 
   def update
+    @video = Video.find(params[:video_id])
+    @comment = @video.comments.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to video_path(@video)
   end
 
   def destroy
-  end
-
-  def delete
+    @video = Video.find(params[:video_id])
+    @comment = @video.comments.find(params[:id])
+    @comment.destroy
+    redirect_to video_path(@video)
   end
 
   private
