b93dd53ab0d32d51a96c84a3e5e7051578865221
gabrielhicks <gabrielhicks@mail.com>
2020-09-30 18:13:11 -0400

    work on pills

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,7 @@ class Video < ApplicationRecord
     has_many :playlist_videos
     has_many :playlists, through: :playlist_videos
     has_many :comments
+    validates :category, presence: true
 
     def category_videos
         video_array = []
