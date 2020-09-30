04db909677ff6f7e3b4750d8b648dbcbbc1e5847
gabrielhicks <gabrielhicks@mail.com>
2020-09-30 15:08:43 -0400

    Merging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user.update_attributes(user_params)
+    if @user.update(user_params)
       flash[:success] = "Profile Updated!"
       redirect_to @user
     else
