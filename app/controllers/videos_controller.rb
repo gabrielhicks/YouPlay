865448ebb50c248ffe527e07f0f85a83b2b37db0
gabrielhicks <gabrielhicks@mail.com>
2020-09-30 09:29:49 -0400

    combatting no video id issue

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -17,13 +17,16 @@ class VideosController < ApplicationController
   def handle_search
     @key = ENV['API_KEY']
     @input = (params[:input])
-    @max = '20'
+    @max = '30'
     @playlist = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults='+@max+'&q='+@input+'&key='+@key)
     array = JSON.parse(@playlist)
 
     i = 0
     @max.to_i.times do
-      Video.create(video_id: array["items"][i]["id"]["videoId"], title: array["items"][i]["snippet"]["title"], thumbnail_url: array["items"][i]["snippet"]["thumbnails"]["medium"]["url"], category:@input)
+      newvid = Video.new(video_id: array["items"][i]["id"]["videoId"], title: array["items"][i]["snippet"]["title"], thumbnail_url: array["items"][i]["snippet"]["thumbnails"]["medium"]["url"], category:@input)
+      if newvid.video_id != nil
+        newvid.save
+      end
       i+=1
     end
     redirect_to videos_path
