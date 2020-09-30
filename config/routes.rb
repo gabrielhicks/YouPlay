3b18a01ce55b150cbf70475998ad978f49beebe9
zhengjianliu <comezheng@gmail.com>
2020-09-30 16:06:55 -0400

    update

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,12 @@ Rails.application.routes.draw do
   resources :playlists
   resources :comments
   resources :playlist_videos
+
   get '/search', to: "videos#index", as: "search"
   post '/search', to: "videos#handle_search"
   get "/login", to: "users#login", as: "login"
   post "/login", to: "users#handle_login"
   delete "/logout", to: "users#logout"
+
+  root 'videos#index'
 end
