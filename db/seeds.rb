0370c9ae49cebf17b9c5a7ac962c24e6c5cbd053
gabrielhicks <gabrielhicks@mail.com>
2020-10-02 09:30:09 -0400

    Adding seeds and fixing footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,29 +13,29 @@ require_relative '../config/environment'
 # query = ["Ruby+on+Rails", "Java+Programming","JavaScript+ES6","Reactjs+Tutorial","Nodejs+Tutorial", "Expressjs+Tutorial", "Laravel+Tutorial", "PHP+Tutorial", "Bootstrap+CSS+Tutorial","Vanilla+CSS+Tutorial"]
 
 @youtube1 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=Ruby+on+Rails&key='+@key)
-# @youtube2 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=Java+Programming+Tutorial&key='+@key)
-# @youtube3 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=JavaScript+ES6+Tutorial&key='+@key)
-# @youtube4 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=Reactjs+Tutorial&key='+@key)
-# @youtube5 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=Nodejs+Tutorial&key='+@key)
-# @youtube6 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=Expressjs+Tutorial&key='+@key)
-# @youtube7 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=Laravel+Tutorial&key='+@key)
-# @youtube8 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=PHP+Tutorial&key='+@key)
-# @youtube9 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=Bootstrap+CSS+Tutorial&key='+@key)
-# @youtube10 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=Vanilla+CSS+Tutorial&key='+@key)
+@youtube2 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=Java+Programming+Tutorial&key='+@key)
+@youtube3 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=JavaScript+ES6+Tutorial&key='+@key)
+@youtube4 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=Reactjs+Tutorial&key='+@key)
+@youtube5 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=Nodejs+Tutorial&key='+@key)
+@youtube6 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=Expressjs+Tutorial&key='+@key)
+@youtube7 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=Laravel+Tutorial&key='+@key)
+@youtube8 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=PHP+Tutorial&key='+@key)
+@youtube9 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=Bootstrap+CSS+Tutorial&key='+@key)
+@youtube10 = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=Vanilla+CSS+Tutorial&key='+@key)
 
 @yt1 = JSON.parse(@youtube1)
-# @yt2 = JSON.parse(@youtube2)
-# @yt3 = JSON.parse(@youtube3)
-# @yt4 = JSON.parse(@youtube4)
-# @yt5 = JSON.parse(@youtube5)
-# @yt6 = JSON.parse(@youtube6)
-# @yt7 = JSON.parse(@youtube7)
-# @yt8 = JSON.parse(@youtube8)
-# @yt9 = JSON.parse(@youtube9)
-# @yt10 = JSON.parse(@youtube10)
+@yt2 = JSON.parse(@youtube2)
+@yt3 = JSON.parse(@youtube3)
+@yt4 = JSON.parse(@youtube4)
+@yt5 = JSON.parse(@youtube5)
+@yt6 = JSON.parse(@youtube6)
+@yt7 = JSON.parse(@youtube7)
+@yt8 = JSON.parse(@youtube8)
+@yt9 = JSON.parse(@youtube9)
+@yt10 = JSON.parse(@youtube10)
 
-# @yt_array = [@yt1, @yt2, @yt3, @yt4, @yt5, @yt6, @yt7, @yt8, @yt9, @yt10]
-@yt_array = [@yt1]
+@yt_array = [@yt1, @yt2, @yt3, @yt4, @yt5, @yt6, @yt7, @yt8, @yt9, @yt10]
+# @yt_array = [@yt1]
 
 j=0
 @yt_array.each do |array|
