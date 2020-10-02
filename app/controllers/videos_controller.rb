e55b5b420c1fe04d1caeef2d1294c10afb14c873
gabrielhicks <gabrielhicks@mail.com>
2020-10-02 13:36:13 -0400

    updates

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -24,21 +24,20 @@ class VideosController < ApplicationController
   def handle_search
     @videos = Video.all
     @key = ENV['API_KEY']
-    @input = (params[:input])
-    @category = (params[:category])
-    @max = '30'
-    @playlist = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults='+@max+'&q='+@input+'&key='+@key)
-    array = JSON.parse(@playlist)
+    @input = (params[:input]).downcase
+    @category = (params[:category]).downcase
+    # @max = '30'
+    # @playlist = RestClient.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults='+@max+'&q='+@input+'&key='+@key)
+    # array = JSON.parse(@playlist)
 
-    i = 0
-    @max.to_i.times do
-      newvid = Video.new(video_id: array["items"][i]["id"]["videoId"], title: array["items"][i]["snippet"]["title"], thumbnail_url: array["items"][i]["snippet"]["thumbnails"]["medium"]["url"], category:@input)
-      if newvid.video_id != nil
-        newvid.save
-      end
-      i+=1
-    end
-    # redirect_to videos_path
+    # i = 0
+    # @max.to_i.times do
+    #   newvid = Video.new(video_id: array["items"][i]["id"]["videoId"], title: array["items"][i]["snippet"]["title"], thumbnail_url: array["items"][i]["snippet"]["thumbnails"]["medium"]["url"], category:@input)
+    #   if newvid.video_id != nil
+    #     newvid.save
+    #   end
+    #   i+=1
+    # end
   end
 
   def all
