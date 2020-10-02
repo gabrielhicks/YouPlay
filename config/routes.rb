679dfd67fd80f16aab5280bf6da79f1b608d079e
gabrielhicks <gabrielhicks@mail.com>
2020-10-02 11:23:23 -0400

    Minor fix

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,10 @@ Rails.application.routes.draw do
   resources :playlist_videos
   get '/search', to: "videos#index", as: "search"
   post '/search', to: "videos#handle_search"
+  get '/search', to: "videos#handle_search"
   get "/login", to: "users#login", as: "login"
   post "/login", to: "users#handle_login"
   delete "/logout", to: "users#logout"
   get "/all", to: "videos#all", as: "all_videos"
-
   root 'videos#index'
 end
