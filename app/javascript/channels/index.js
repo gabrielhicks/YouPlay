e4c35a9c7d2bca2c957e8266d670627584958618
gabrielhicks <gabrielhicks@mail.com>
2020-10-05 11:41:56 -0400

    Moved js

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,13 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
